1cbbef7b9ee76efe6a9d7a439f50ab82b8906f65
Rafał Kuchta <rafalmax12@interia.pl>
2019-03-29 12:27:59 +0100

    change tasks

--- src/component/App.js
+++ src/component/App.js
@@ -9,80 +9,8 @@ class App extends Component {
     tasks: [
       {
         id: 0,
-        tekst: "Zrobić Kurs",
-        date: "2019-03-03",
-        active: true,
-        important: false,
-        finichDate: null
-      },
-      {
-        id: 1,
-        tekst: "Zrobić Kurs 1",
-        date: "2019-03-03",
-        active: true,
-        important: true,
-        finichDate: null
-      },
-      {
-        id: 2,
-        tekst: "Zrobić Kurs 2",
-        date: "2019-03-03",
-        active: true,
-        important: true,
-        finichDate: null
-      },
-      {
-        id: 3,
-        tekst: "Zrobić Kurs 3",
-        date: "2019-03-03",
-        active: true,
-        important: true,
-        finichDate: null
-      },
-      {
-        id: 4,
-        tekst: "Zrobić Kurs 4",
-        date: "2019-03-03",
-        active: true,
-        important: false,
-        finichDate: null
-      },
-      {
-        id: 5,
-        tekst: "Zrobić Kurs 5",
-        date: "2019-03-03",
-        active: true,
-        important: true,
-        finichDate: null
-      },
-      {
-        id: 6,
-        tekst: "Zrobić Kurs 6",
-        date: "2019-03-03",
-        active: true,
-        important: false,
-        finichDate: null
-      },
-      {
-        id: 7,
-        tekst: "Zrobić Kurs 7",
-        date: "2019-03-03",
-        active: true,
-        important: false,
-        finichDate: null
-      },
-      {
-        id: 8,
-        tekst: "Zrobić Kurs 8",
-        date: "2019-03-03",
-        active: true,
-        important: true,
-        finichDate: null
-      },
-      {
-        id: 9,
-        tekst: "Zrobić Kurs 9",
-        date: "2019-03-03",
+        tekst: "Zadanie numer 1",
+        date: "2019-04-03",
         active: true,
         important: false,
         finichDate: null
